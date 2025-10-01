c8c69e4a75bd756d8d54dd76849cbf0db3acf490
Matheus Santana <mssantana@ecomp.uefs.br>
2025-10-01 14:58:22 -0300

    update file

--- EduPass/src/app/Login.tsx
+++ EduPass/src/app/Login.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Button } from '../components/Button';
+import { Input } from '../components/Input';
 
 
 export default function home() {
@@ -8,6 +9,8 @@ export default function home() {
     <><View style={styles.container}>
       <Text style={styles.title}>Bem-vindo ao EduPass!</Text>
     </View>
+      <Input placeholder="CPF" keyboardType="email-address"/>
+      <Input placeholder="Senha" secureTextEntry />
     <View style={styles.container}>
         <Button title="Entrar" onPress={() => alert('Login efetuado!')} />
       </View></>
